ec7e2a790a178369179a9628d8641dd28d8d6bc8
tuanht12 <thanhtuan97dn@gmail.com>
2025-10-04 11:30:17 +0700

    Revert SERVER_HOST to static IP address for consistency

--- src/configs.py
+++ src/configs.py
@@ -1,6 +1,3 @@
-from utils import get_local_ip
-
-
-SERVER_HOST = get_local_ip()
+SERVER_HOST = "127.0.0.1"  # Change this to IPV4 address of your server
 SERVER_PORT = 65432
 DEFAULT_BUFFER_SIZE = 1024
